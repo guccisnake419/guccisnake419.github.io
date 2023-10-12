a3150fbddefbc238810bcc94bb772dcfa1c1ba6b
Victor Savage <victorsavage03@gmail.com>
2023-10-11 22:09:41 -0500

    10/11/23

---
+++ functions.js
@@ -0,0 +1,88 @@
+function makeRandBox(){
+    let randnum= Math.floor(Math.random()*9);
+    console.log(randnum);
+    let colorvalOne= Math.floor(Math.random()*360);
+    let colorvalTwo = Math.floor(Math.random()*100)
+    let colorvalThree = Math.floor(Math.random()*100)
+    let randVal= (Math.floor(Math.random()*5)+1)*10
+    let comColor= 'hsl('+ colorvalOne+','+colorvalTwo+'%,'+colorvalThree+'%)';
+    let randColor= 'hsl('+ colorvalOne+','+(colorvalTwo-randVal)+'%,'+colorvalThree+'%)'
+    const nodelist=document.querySelectorAll(".inner1")
+    for(let i=0; i<9;i++){
+        nodelist[i].style.backgroundColor=comColor;
+    }
+    nodelist[randnum].style.backgroundColor=randColor;
+
+    
+}
+
+function checkStat(event){//checks if a selected box is the answer
+    let nodelist = document.querySelectorAll('.inner1')
+    let count =0;
+    let isright = false;
+    for(let i=0;i<9;i++){
+        
+        if(event.target.style.backgroundColor==nodelist[i].style.backgroundColor){
+            count++;
+        }
+
+    }
+    if(count>1){
+        isright=false
+    }
+    else{
+        isright =true
+    }
+
+    if(isright){
+        alert('You are a right')
+        levelnum++;
+        document.getElementById('level_played').innerHTML=levelnum;
+        makeRandBox();
+
+
+
+        
+    }
+    else{
+        alert('Try again')
+        Person.score= levelnum;
+        levelnum=1;
+        ReactDOM.unmountComponentAtNode(document.getElementById("root"));
+        gameoverseq();
+    }
+    
+}
+
+function replaybutton(){
+    ReactDOM.unmountComponentAtNode(document.getElementById("root"));
+    playgame();
+
+}
+
+function gameoverseq(){
+    const gamebox = (
+        <div className="gameoverbox" >
+            <h3>{Person.name.toUpperCase()}'S SCORE: {Person.score}</h3>
+            <h3>HIGHSCORE {Person.highscore}</h3>
+            <button type="button" onClick={replaybutton}>Play Again</button>
+        </div>
+    );
+    const gameboxhighscore = (
+        <div className="gameoverbox" >
+            <h3>NEW HIGHSCORE FROM {Person.name.toUpperCase()}</h3>
+            <p><h3> {Person.score}</h3></p>
+            <button type="button" onClick={replaybutton}>PLAY AGAIN</button>
+        </div>
+    );
+    if(Person.score> Person.highscore){
+        Person.highscore= Person.score
+        ReactDOM.render([gameboxhighscore], document.getElementById("root"));
+    }
+    else{
+        
+        ReactDOM.render([gamebox], document.getElementById("root"));
+    }
+    
+    
+}
--- script.js
+++ script.js
@@ -1,87 +1,88 @@
+
+
 //TODO: 
 //1. Implement client side storage to store name and high score
 //2. Add homepage/landing page that askes for client's name
 //3. Add gameover sequence, that resets the whole game
-
-const title = React.createElement("h1", null, "GUESS THE DIFFERENT BOX");
-const gamebox = (
-    <div className="outer_box" >
-        <div className="row_1" id="flex">
-            <div className="inner1 box_one box1" onClick={checkStat}></div>
-            <div className="inner1 diffbox box_two box2" onClick={checkStat}></div>
-            <div className="inner1 box_three box3" onClick={checkStat}></div>
-        </div>
-        <div className="row_2" id="flex">
-            <div className="inner1 box_four box4" onClick={checkStat}></div>
-            <div className="inner1 box_five box5" onClick={checkStat}></div>
-            <div className="inner1 box_six box6" onClick={checkStat}></div>
-        </div>
-        <div className="row_3" id="flex">
-            <div className="inner1 box_seven box7" onClick={checkStat}></div>
-            <div className="inner1 box_eight box8" onClick={checkStat}></div>
-            <div className="inner1 box_nine box9" onClick={checkStat}></div>
-        </div>
-   
-    </div>
-);
 let levelnum= 1;
-const level=(
-    <h2 id ="level">
-        LEVEL  &nbsp;
-        <span id= "level_played">{levelnum}</span>
-    </h2>
-) 
-
-ReactDOM.render([title, gamebox,level], document.getElementById("root"));
-
-function makeRandBox(){
-    let randnum= Math.floor(Math.random()*9);
-    console.log(randnum);
-    let colorvalOne= Math.floor(Math.random()*360);
-    let colorvalTwo = Math.floor(Math.random()*100)
-    let colorvalThree = Math.floor(Math.random()*100)
-    let randVal= (Math.floor(Math.random()*5)+1)*10
-    let comColor= 'hsl('+ colorvalOne+','+colorvalTwo+'%,'+colorvalThree+'%)';
-    let randColor= 'hsl('+ colorvalOne+','+(colorvalTwo-randVal)+'%,'+colorvalThree+'%)'
-    const nodelist=document.querySelectorAll(".inner1")
-    for(let i=0; i<9;i++){
-        nodelist[i].style.backgroundColor=comColor;
-    }
-    nodelist[randnum].style.backgroundColor=randColor;
-
+function playgame(){
+    // const title = React.createElement("h1", null, "GUESS THE DIFFERENT BOX");
+    const gamebox = (
+        <div className="outer_box" >
+            <div className="row_1" id="flex">
+                <div className="inner1 box_one box1" onClick={checkStat}></div>
+                <div className="inner1 diffbox box_two box2" onClick={checkStat}></div>
+                <div className="inner1 box_three box3" onClick={checkStat}></div>
+            </div>
+            <div className="row_2" id="flex">
+                <div className="inner1 box_four box4" onClick={checkStat}></div>
+                <div className="inner1 box_five box5" onClick={checkStat}></div>
+                <div className="inner1 box_six box6" onClick={checkStat}></div>
+            </div>
+            <div className="row_3" id="flex">
+                <div className="inner1 box_seven box7" onClick={checkStat}></div>
+                <div className="inner1 box_eight box8" onClick={checkStat}></div>
+                <div className="inner1 box_nine box9" onClick={checkStat}></div>
+            </div>
     
+        </div>
+    );
+    
+    const level=(
+        <div>
+        <h2 id ="level">
+            LEVEL  &nbsp;
+            <span id= "level_played">{levelnum}</span>
+
+        </h2>
+        <p>
+            <h2>HIGHSCORE &nbsp;
+            <span id= "level_played">{Person.highscore}</span></h2>
+        </p>
+        </div>
+        
+    ) 
+
+    ReactDOM.render([gamebox,level], document.getElementById("root"));
+
+
+    makeRandBox();
+
 }
-makeRandBox();
-function checkStat(event){//checks if a selected box is the answer
-    let nodelist = document.querySelectorAll('.inner1')
-    let count =0;
-    let isright = false;
-    for(let i=0;i<9;i++){
-        
-        if(event.target.style.backgroundColor==nodelist[i].style.backgroundColor){
-            count++;
-        }
 
-    }
-    if(count>1){
-        isright=false
-    }
-    else{
-        isright =true
-    }
+//handles the first page to get the name 
+  
+function handleSubmit(){
+    alert('A name was submitted: ' + player.value);
+    Person.name= player.value
+    ReactDOM.unmountComponentAtNode(document.getElementById("root"));
+    playgame();
 
-    if(isright){
-        alert('You are a right')
-        levelnum++;
-        document.getElementById('level_played').innerHTML=levelnum;
-        makeRandBox();
+}
+const Person ={
+    name:"",
+    score:0,
+    highscore :0
+}
+const Game= {
+    player: getName(),
+}
 
+function getName(){
 
+    const namebox=(
+        <div id="namebox">
+            <p><h3>ENTER YOUR NAME</h3></p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input type='text' id='player' ></input>
+                    <input type='submit'></input>
+                </label>
+            </form>
+            
+        </div>
+    )
+    ReactDOM.render([namebox], document.getElementById("root"));
 
-        
-    }
-    else{
-        alert('Try again')
-    }
-    
+    return document.getElementById("name").value;
 }
